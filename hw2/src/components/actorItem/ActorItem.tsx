a27a79657bb234707781368031a748677ad78ea2
Sherkhan <sherkhanazim@gmail.com>
2022-03-15 09:31:04 +0600

    add button

--- hw2/src/components/actorItem/ActorItem.tsx
+++ hw2/src/components/actorItem/ActorItem.tsx
@@ -1,19 +1,25 @@
-import { Actor } from '../../types'
+import React, { useState } from 'react';
+import { Actor } from '../../types';
 
 type Props = {
   actor: Actor;
 }
 
 const ActorItem: React.FC<Props> = ({ actor }) => {
+  const [isFollowed, setIsFollowed] = useState<boolean>(false);
+  const followClick = () => {
+    setIsFollowed(!isFollowed);
+  }
+
   return (
     <div className="movie-item" style={{ marginBottom: '1rem' }}>
       <img src={actor.image} alt={actor.name} style={{ width: '200px' }} />
       <div>{actor.name}</div>
       <div>{actor.movies}</div>
       <div>{actor.age} y. o.</div>
-      {/* <button onClick={watchLaterClick}>
-        {state.addedToWatchLater ? 'Added to list' : 'Watch later'}
-      </button> */}
+      <button onClick={followClick}>
+        {isFollowed ? 'Following' : 'Follow'}
+      </button>
     </div>
   );
 }
