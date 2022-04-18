0e68e0790ef285d1e364e7de9284dacbaf894afc
Sherkhan <sherkhanazim@gmail.com>
2022-04-18 18:33:50 +0600

    complete hw8 (delete student)

--- src/pages/students/students-list/StudentsList.tsx
+++ src/pages/students/students-list/StudentsList.tsx
@@ -1,16 +1,33 @@
 import { useContext, useEffect } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { StudentContext } from "../../../contexts/StudentContext";
-import { getStudents } from "../../../services/students";
+import { deleteStudent, getStudents } from "../../../services/students";
 
 const StudentsList = () => {
   const { state, dispatch } = useContext(StudentContext);
+  const navigate = useNavigate();
 
   useEffect(() => {
     getStudents().then((res) => {
       dispatch({ type: "GET_STUDENTS", payload: res });
     });
-  }, []);
+  }, [state]);
+
+  const deleteStudentButton = (id: string) => {
+    deleteStudent(id)
+      .then((res) => {
+        dispatch({ type: "DELETE_STUDENT", payload: "Student was deleted!" });
+        if (res.id) {
+          navigate("/students");
+        }
+      })
+      .catch((err) => {
+        dispatch({
+          type: "DELETE_STUDENT",
+          payload: "Student deletion failed!",
+        });
+      });
+  };
 
   return (
     <div>
@@ -40,6 +57,7 @@ const StudentsList = () => {
               <td>
                 <Link to={`/students/${student.id}`}>View</Link>
                 <Link to={`/students/edit/${student.id}`}>Edit</Link>
+                <button onClick={() => deleteStudentButton(student.id)}>Delete</button>
               </td>
             </tr>
           ))}
